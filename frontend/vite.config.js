f37301880cceb15d309c85f8544d45b2b422d427
Gungun Rathore <rathoregungun200406@gmail.com>
2026-04-18 17:03:48 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://expense-tracker-backend-9nrf.onrender.com',
         changeOrigin: true,
       },
     },
